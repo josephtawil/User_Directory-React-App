8d39f58eef5999204f97a708244ff9bb59d46122
Joseph <jtawil2015@gmail.com>
2020-08-29 13:03:13 -0800

    Updates

---
+++ precache-manifest.7fa41ec988e3f35e5875a78471ec5407.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0f307e580ba570f18f89693433d062f",
+    "url": "/User_Directory-React-App/index.html"
+  },
+  {
+    "revision": "b39ef6551dbcc64a61d6",
+    "url": "/User_Directory-React-App/static/css/main.3fcd11d6.chunk.css"
+  },
+  {
+    "revision": "b28d945764db004c4384",
+    "url": "/User_Directory-React-App/static/js/2.d36225b7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/User_Directory-React-App/static/js/2.d36225b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b39ef6551dbcc64a61d6",
+    "url": "/User_Directory-React-App/static/js/main.8705ca45.chunk.js"
+  },
+  {
+    "revision": "8abf4ee0142047af1520",
+    "url": "/User_Directory-React-App/static/js/runtime-main.bbc8389d.js"
+  }
+]);
